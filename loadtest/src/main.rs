83a5a48ce38af0a58be998555c31a3ce92faa2a6
Jêrome Rouaix <jerome@beezup.com>
2020-09-10 22:54:27 +0200

    WIP

--- loadtest/src/main.rs
+++ loadtest/src/main.rs
@@ -5,11 +5,20 @@ async fn loadtest_uniques(user: &GooseUser) -> GooseTaskResult {
     Ok(())
 }
 
+async fn loadtest_ping(user: &GooseUser) -> GooseTaskResult {
+    let _goose = user.get("/ping/").await?;
+    Ok(())
+}
+
 fn main() -> Result<(), GooseError> {
     GooseAttack::initialize()?
         .register_taskset(
             taskset!("LoadtestTasks")
                 .register_task(task!(loadtest_uniques)),
+        )
+        .register_taskset(
+            taskset!("ComparablePingTasks")
+                .register_task(task!(loadtest_ping)),
         )
         .execute()?
         .print();
